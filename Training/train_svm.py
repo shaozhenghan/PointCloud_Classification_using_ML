c4742643e990824595b83b8b986f4857d4263cf0
shaozhenghan <szh13964751398@163.com>
2018-08-18 13:14:04 +0200

    modify train_svm.py

--- Training/train_svm.py
+++ Training/train_svm.py
@@ -8,6 +8,7 @@ import numpy as np
 from sklearn import svm
 from sklearn import preprocessing
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import fbeta_score, make_scorer
 
 # load data for training
 feature_matrix = np.loadtxt('/media/shao/TOSHIBA EXT/data_object_velodyne/feature_matrix_with_label/train/data/r_0.16.txt')
@@ -24,12 +25,16 @@ normalizer = preprocessing.Normalizer(norm='l2', copy=False)
 normalizer.fit_transform(data)
 
 # train svm using grid search
-params = {'kernel':['rbf', 'linear'], 'C':[0.001, 0.01, 0.1, 1], 'gamma':[0.001, 0.01, 0.1, 1]}
-# params = {'kernel':['rbf'], 'C':[1], 'gamma':[1]}
+fone_scorer = make_scorer(fbeta_score, beta=1, average='weighted')
+# params = {  'kernel':['rbf', 'linear'], 
+#             'C':[0.5, 1.0, 1.3, 1.5, 2.0, 2.1, 2.2, 2.3, 2.4], 
+#             'gamma':[0.5, 1.0, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
+#             }
+params = {'kernel':['rbf'], 'C':[2.3], 'gamma':[1.4]}
 svc = svm.SVC()
 clf = GridSearchCV(
-    svc, params, scoring='accuracy', 
-    n_jobs=5, cv=5, return_train_score=False)
+    svc, params, scoring=fone_scorer,
+    n_jobs=5, cv=5, return_train_score=False, iid=True)
 clf.fit(data, target)
 
 # print important info
