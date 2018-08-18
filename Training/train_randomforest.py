261eb35561b07e484626a85931f84c220d75e65f
shaozhenghan <szh13964751398@163.com>
2018-08-18 17:40:49 +0200

    modify train_randomforest.py

--- Training/train_randomforest.py
+++ Training/train_randomforest.py
@@ -7,9 +7,13 @@
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np 
 
-feature_matirx = np.loadtxt('/media/shao/TOSHIBA EXT/data_object_velodyne/feature_matrix_with_label/train/data/r_0.16.txt')
-data = feature_matirx[:, :-1]
-target = feature_matirx[:, -1]
+# load data for training
+feature_matrix = np.loadtxt('/media/shao/TOSHIBA EXT/data_object_velodyne/feature_matrix_with_label/train/data/r_0.16.txt')
+print('the shape of the loaded feature matrix is ', feature_matrix.shape)
+data = feature_matrix[:, :-1]
+target = feature_matrix[:, -1]
+
+# set random forest params
 rfc = RandomForestClassifier(   max_depth=10, 
                                 random_state=0, 
                                 n_estimators=10,
@@ -17,8 +21,13 @@ rfc = RandomForestClassifier(   max_depth=10,
                                 oob_score=True,
                                 n_jobs=-1,
                                 bootstrap=True,
-                                class_weight='balanced')
+                                class_weight='balanced'
+                            )
+
+# train random forest 
 rfc.fit(data, target)
+
+# print important info
 print(rfc.feature_importances_)
 print(rfc.oob_score_)
 
